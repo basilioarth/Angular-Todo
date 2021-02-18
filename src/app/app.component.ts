fe7b7f93f72ff5e87740c2c070eaff5db0024e5c
basilio.arth <basilio.arth@gmail.com>
2021-02-18 12:08:57 -0300

    Adicionando Concluir, Refazer, Remover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root', 
@@ -13,5 +14,34 @@ import { Component } from '@angular/core';
   // É um array onde definimos todos os css que esse componente tem
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  // todos é o nome da nossa variável.
+  // public indica que essa variável pode ser acessada por todos, inclusive lá no front pelo html.
+  // usamos o : para realizarmos a tipagem da nossa variável.
+  // any[] significa que a variável será um array/lista de qualquer coisa.
+  // = [] inicializa a nossa variável como um array vazio.
+  public title: String = 'Minhas Trefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', true));
+    this.todos.push(new Todo(2, 'Cortar o cabelo', false));
+    this.todos.push(new Todo(3, 'Ir ao mercado', false));
+  }
+  // Método que é chamado toda vez que o componente inicia.
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    // retorna o índice do elemento da lista 'todos' que seja igual ao 'todo' passado como parâmetro. Se não encontrar ninguém, retorna -1.
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
